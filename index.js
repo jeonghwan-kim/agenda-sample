4dafbb8e66e6cddf46bae3286c5145ae361249e2
jeonghwan <ej88ej@gmail.com>
2015-12-09 09:47:52 +0900

    Add more job

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 'use strict';
 
 var Agenda = require('agenda');
-var agenda = new Agenda({db: { address: 'localhost:27017/agenda-sample'}});
+var agenda = new Agenda({
+  db: {
+    address: 'localhost:27017/agenda-sample'
+  },
+  processEvery: '10 seconds'
+});
 
-agenda.define('tick', function(job, done) {
-  console.log('agenda sample ' + job.attrs.data.by);
+agenda.define('1tick', function(job, done) {
+  console.log('1 tick by ' + job.attrs.data.by);
   done();
 });
 
-agenda.every('3 seconds', 'tick', {by: 'chris'});
+agenda.define('3tick', function(job, done) {
+  console.log('3 tick by ' + job.attrs.data.by);
+  done();
+});
 
-agenda.start();
+agenda.define('send email', function(job, done) {
+  console.log('send email');
+  done();
+});
+
+
+agenda.on('ready', function () {
+  agenda.every('1 second', '1tick', {by: 'chris'});
+  agenda.every('3 second', '3tick', {by: 'chris'});
+  agenda.now('send email');
+  agenda.start();
+});
+
 
